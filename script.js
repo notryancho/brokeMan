9f17a3b5a1a031f2854762bbd54c0510de4dae3e
notryancho <ryancho1226@hotmail.com>
2023-01-25 00:24:09 -0500

    added a message after player wins

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const alphabetDiv = document.getElementById("alphabet");
 const balance = document.getElementById("balance");
 const bankBalance = 100;
 const correctLettersDiv = document.getElementById("correctLetters");
+const youWon = document.getElementById("youWon");
 
+for(let i=0; i < word.length; i++){
+    let correctLetterSpan = document.createElement("span");
+    correctLetterSpan.innerHTML = "_";
+    correctLettersDiv.appendChild(correctLetterSpan);
+}
 alphabetArray.forEach(function(letter) {
     let button = document.createElement("button");
     button.innerHTML = letter;
@@ -16,12 +22,22 @@ alphabetArray.forEach(function(letter) {
         button.setAttribute("disabled", "true");
         button.style.visibility = "hidden";
       if (wordArray.indexOf(button.innerHTML) !== -1) {
-        let correctLetterSpan = document.createElement("span");
-        correctLetterSpan.innerHTML = button.innerHTML;
-        correctLettersDiv.appendChild(correctLetterSpan);
+        for (let i = 0; i < wordArray.length; i++) {
+            if (wordArray[i] === button.innerHTML){
+                let correctLetterSpan = correctLettersDiv.childNodes[i];
+                correctLetterSpan.innerHTML = button.innerHTML;
+            }
+        }
       } else {
         bankBalance -= 10;
         balance.innerHTML = "Bank Balance: $" + bankBalance;
+      }
+      let correctLetters = "";
+      for (let i = 0; i < correctLettersDiv.childNodes.length; i++) {
+        correctLetters += correctLettersDiv.childNodes[i].innerHTML;
+      }
+      if (correctLetters === word) {
+        youWon.innerHTML = "NICE, YOU'RE STILL RICH!"
       }
     });
   });
