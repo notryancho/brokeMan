85f4df965110b85d28cf4a0806bf41a81af22b94
notryancho <ryancho1226@hotmail.com>
2023-01-24 22:58:18 -0500

    deleted submit option

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const word = "javascript";
-const guess = document.getElementById("guess");
-const submit = document.getElementById("submit");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphabetArray = alphabet.split("");
+const wordArray = word.split("");
+const alphabetDiv = document.getElementById("alphabet");
 const balance = document.getElementById("balance");
 const bankBalance = 100;
 
-submit.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (word.indexOf(guess.value) === -1) {
-    bankBalance -= 10;
-    balance.innerHTML = "Bank Balance: $" + bankBalance;
-  }
-});
+alphabetArray.forEach(function(letter) {
+    let button = document.createElement("button");
+    button.innerHTML = letter;
+    alphabetDiv.appendChild(button);
+    button.addEventListener("click", function() {
+      if (wordArray.indexOf(button.innerHTML) !== -1) {
+        alert("Correct letter!");
+      } else {
+        bankBalance -= 10;
+        balance.innerHTML = "Bank Balance: $" + bankBalance;
+      }
+    });
+  });
+  
+  
+  
 
 
