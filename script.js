a90cb3da0b1515f4aa7e7d2959a6f438a6274da3
notryancho <ryancho1226@hotmail.com>
2023-01-24 22:44:39 -0500

    Defining id and create button

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+const word = "javascript";
+const guess = document.getElementById("guess");
+const submit = document.getElementById("submit");
+const balance = document.getElementById("balance");
+const bankBalance = 100;
+
+submit.addEventListener("click", function(e) {
+  e.preventDefault();
+  if (word.indexOf(guess.value) === -1) {
+    bankBalance -= 10;
+    balance.innerHTML = "Bank Balance: $" + bankBalance;
+  }
+});
 
 
