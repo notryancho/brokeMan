49e6d45ed22fc907744b421dd155262f4c2c0164
notryancho <ryancho1226@hotmail.com>
2023-01-24 23:32:02 -0500

    displayed the correct letters

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const wordArray = word.split("");
 const alphabetDiv = document.getElementById("alphabet");
 const balance = document.getElementById("balance");
 const bankBalance = 100;
+const correctLettersDiv = document.getElementById("correctLetters");
 
 alphabetArray.forEach(function(letter) {
     let button = document.createElement("button");
     button.innerHTML = letter;
     alphabetDiv.appendChild(button);
     button.addEventListener("click", function() {
+        button.setAttribute("disabled", "true");
+        button.style.visibility = "hidden";
       if (wordArray.indexOf(button.innerHTML) !== -1) {
-        alert("Correct letter!");
+        let correctLetterSpan = document.createElement("span");
+        correctLetterSpan.innerHTML = button.innerHTML;
+        correctLettersDiv.appendChild(correctLetterSpan);
       } else {
         bankBalance -= 10;
         balance.innerHTML = "Bank Balance: $" + bankBalance;
