b94ad22100e8b9c950e8f6dd5d303a91dfcb0398
notryancho <ryancho1226@hotmail.com>
2023-01-26 00:48:24 -0500

    created replay button

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const alphabetDiv = document.getElementById("alphabet");
 // const bankBalance = 100;
 const correctLettersDiv = document.getElementById("correctLetters");
 const message = document.getElementById("message");
+const replayBtn = document.createElement("button");
 
-let bankBalance = 50;
+let bankBalance = 70;
 const balance = document.getElementById("balance");
 balance.innerHTML = "Bank Balance: $" + bankBalance;
 
@@ -24,7 +25,7 @@ alphabetArray.forEach(function(letter) {
     button.innerHTML = letter;
     alphabetDiv.appendChild(button);
     button.addEventListener("click", function() {
-        button.setAttribute("disabled", "true");
+    button.setAttribute("disabled", "true");
       if (wordArray.indexOf(button.innerHTML) === -1) {
         bankBalance -= 10;
         balance.innerHTML = "Bank Balance: $" + bankBalance;
@@ -33,7 +34,13 @@ alphabetArray.forEach(function(letter) {
             for (let i = 0; i < alphabetDiv.childNodes.length; i++) {
                 alphabetDiv.childNodes[i].setAttribute("disabled", "true");
             }
+            replayBtn.innerHTML = "Replay";
+        document.getElementById("game").appendChild(replayBtn);
+        replayBtn.addEventListener("click", function() {
+        location.reload();
+    });
         }
+    
       } else {
         for (let i = 0; i < wordArray.length; i++) {
             if (wordArray[i] === button.innerHTML){
@@ -50,46 +57,34 @@ alphabetArray.forEach(function(letter) {
       }
       if (correctLetters === word) {
         message.innerHTML = "NICE, YOU'RE STILL RICH!"
+        replayBtn.innerHTML = "Replay";
+    document.getElementById("game").appendChild(replayBtn);
+    replayBtn.addEventListener("click", function() {
+        location.reload();
+    });
       }
     });
   });
-
-
-
-// for(let i=0; i < word.length; i++){
-//     let correctLetterSpan = document.createElement("span");
-//     correctLetterSpan.innerHTML = "_";
-//     correctLettersDiv.appendChild(correctLetterSpan);
-// }
-// alphabetArray.forEach(function(letter) {
-//     let button = document.createElement("button");
-//     button.innerHTML = letter;
-//     alphabetDiv.appendChild(button);
-//     button.addEventListener("click", function() {
-//         button.setAttribute("disabled", "true");
-//         button.style.visibility = "hidden";
-//       if (wordArray.indexOf(button.innerHTML) !== -1) {
-//         for (let i = 0; i < wordArray.length; i++) {
-//             if (wordArray[i] === button.innerHTML){
-//                 let correctLetterSpan = correctLettersDiv.childNodes[i];
-//                 correctLetterSpan.innerHTML = button.innerHTML;
-//             }
-//         }
-//       } else {
-//         bankBalance -= 10;
-//         balance.innerHTML = "Bank Balance: $" + bankBalance;
-//       }
-//       let correctLetters = "";
-//       for (let i = 0; i < correctLettersDiv.childNodes.length; i++) {
-//         correctLetters += correctLettersDiv.childNodes[i].innerHTML;
-//       }
-//       if (correctLetters === word) {
-//         message.innerHTML = "NICE, YOU'RE STILL RICH!"
-//       }
-//     });
-//   });
   
+// replayBtn.addEventListener("click",function() {
+//     correctLettersDiv.innerHTML = "";
+//     for (let i = 0; i < alphabetDiv.childNodes.length; i++) {
+//         alphabetDiv.childNodes[i].removeAttribute("disabled");
+//     }
+//     word = wordList[Math.floor(Math.random() * wordList.length)];
+//     wordArray = word.split("");
+//     for(let i = 0; i < word.length; i++){
+//         let correctLetterSpan = document.createElement("span");
+//         correctLetterSpan.innerHTML = "_";
+//         correctLettersDiv.appendChild(correctLetterSpan);
+//     }
+//     bankBalance = 70;
+//     balance.innerHTML = "Bank Balance:$" + bankBalance;
+//     message.innerHTML = "";
+//     replayBtn.style.display = "none";
+// });
   
+
   
 
 
